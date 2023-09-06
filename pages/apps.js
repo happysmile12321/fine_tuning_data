4bb0527c6a9a3432181abce78143c83313136a95
happysmile <812024380@qq.com>
2023-09-07 05:23:24 +0800

    init

---
+++ pages/apps.js
@@ -0,0 +1,13 @@
+import { Layout } from "antd";
+const { Sider } = Layout;
+export default function () {
+  return <>
+    <Layout>
+      <Sider
+        style={{
+          height: '100vh',
+        }}
+      >adsf</Sider>
+    </Layout>
+  </>;
+}
